7c994d452aa99f50c3cff95b0f3c67ab352d61ea
Bayazed Hasan <bm.bayzid13@gmail.com>
2025-02-01 16:15:23 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+        screens: {
+            'sm': '567px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+          },
+      extend: {
+        Animation: {
+          "loop-scroll": "loop-scroll 10s linear infinite",
+
+        },
+        keyframes:{
+          "loop-scroll": {
+            from: {transform: "translateX(0)" },
+            to: {transform: "translateX(-100%)" },
+          },
+        },
+
+      },
     },
     plugins: [],
-  }
+  };
